1dab0fe50de1d02ae5d67f5799f91af998fdb1c6
Peter-Sten Annus <34477656+psannus@users.noreply.github.com>
2019-09-03 15:34:46 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,5 +3,5 @@ function getCoupon() {
     xmlHttp.open("GET", "https://analytics.internationalservicecheck.com/QRCodeGenerator/Baltics_Code.aspx?ctry=ee", false); // false for synchronous request
     xmlHttp.send(null);
     var code = xmlHttp.responseText;
-    document.getElementById("generated-code").innerHTML = code;
+    document.getElementById("generated-code").value = code;
 }
